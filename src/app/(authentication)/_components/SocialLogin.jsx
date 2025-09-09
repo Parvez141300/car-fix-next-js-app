1bccb3b6ca0f1e761b410c599e1c799ba9ff38d7
Parvez141300 <parvezhossain744471@gmail.com>
2025-09-10 02:01:42 +0600

    added social media login funtionality still working on it

--- src/app/(authentication)/_components/SocialLogin.jsx
+++ src/app/(authentication)/_components/SocialLogin.jsx
@@ -1,14 +1,20 @@
+'use client';
+import { signIn } from "next-auth/react";
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 const SocialLogin = () => {
+  const handleSocialLogin = async (providerName) => {
+    const result = await signIn(providerName, {redirect: false});
+    console.log("signin with google or github", result);
+  }
   return (
     <div className="flex flex-col gap-2 mt-4">
-      <button className="btn btn-outline rounded-lg">
+      <button onClick={() => handleSocialLogin('google')} className="btn btn-outline rounded-lg">
         <FcGoogle size={25} /> Login with Google
       </button>
-      <button className="btn btn-outline rounded-lg">
+      <button onClick={() => handleSocialLogin('github')} className="btn btn-outline rounded-lg">
         <FaGithub size={25} /> Login with GitHub
       </button>
     </div>
